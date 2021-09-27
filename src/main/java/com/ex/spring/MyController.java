c92b0589485ced4e98ef22ea67844f608b3f8af7
thd06201 <thd06201@naver.com>
2021-09-27 16:35:47 +0900

    부트스트랩 가져오기 09/27

---
+++ src/main/java/com/ex/spring/MyController.java
@@ -0,0 +1,13 @@
+package com.ex.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/main")
+	public String main() {
+		return "/main";
+	}
+}
